48159039038acd2057b571a1eeb289389754be5f
carmenfu006 <guess_006@hotmail.com>
2019-07-09 12:43:45 +0800

    contact controller rspec

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -33,6 +33,12 @@ class ContactsController < ApplicationController
     end
   end
 
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path
+  end
+
   private
 
   def params_contact
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
   describe 'GET contacts#index' do
-    it 'show all the saved contacts' do
+    it "show all the saved contacts" do
       get :index
       assert_response :success
     end
   end
 
   describe 'GET contacts#new' do
-    it 'assigns fullname, email, phone number and address to new contact' do
+    it "assigns fullname, email, phone number and address to new contact" do
       get :new
       expect(:contact).to have_content(:contact)
     end
@@ -20,25 +20,25 @@ RSpec.describe ContactsController, type: :controller do
   describe 'GET contacts#create' do
     let(:contact_attributes) { attributes_for(:contact) }
 
-    context 'with valid attributes' do
-      it 'save new contact to database' do
+    context "with valid attributes" do
+      it "save new contact to database" do
         expect do
           post :create, params: { contact: contact_attributes }, session: {}
         end.to change(Contact, :count).by(1)
       end
 
-      it 'redirects to contact show page' do
+      it "redirects to contact show page" do
         post :create, params: { contact: contact_attributes }
         expect(response).to redirect_to contact_path(assigns(:contact))
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not save new contact to database' do
+    context "with invalid attributes" do
+      it "does not save new contact to database" do
         expect { post :create, :params => {contact: attributes_for(:contact, :invalid)}}.not_to change(Contact, :count)
       end
 
-      it 're-renders the :new template' do
+      it "re-renders the :new template" do
         post :create, :params => {contact: attributes_for(:contact, :invalid)}
         expect(response).to render_template :new
       end
@@ -52,7 +52,7 @@ RSpec.describe ContactsController, type: :controller do
       expect(assigns(:contact)).to eq contact
     end
 
-    it 'renders the :show template' do
+    it "renders the :show template" do
       contact = create(:contact)
       get :show, params: { id: contact.id }, session: {}
       expect(response).to render_template :show
@@ -66,7 +66,7 @@ RSpec.describe ContactsController, type: :controller do
       expect(assigns(:contact)).to eq contact
     end
 
-    it 'renders the :edit template' do
+    it "renders the :edit template" do
       contact = create(:contact)
       get :edit, params: { id: contact.id }, session: {}
       expect(response).to render_template :edit
@@ -74,6 +74,7 @@ RSpec.describe ContactsController, type: :controller do
   end
 
   describe 'PATCH contact#update' do
+
     let!(:contact) { create(:contact) }
     let(:update_attributes) do {
         full_name: 'Larry',
@@ -82,14 +83,13 @@ RSpec.describe ContactsController, type: :controller do
         address: 'abcdefg'}
       end
 
-    context 'with valid attributes' do
       it "locates the requested @contact" do
         expect do
           patch :update, params: { id: contact.id, contact: update_attributes }, session: {}
         end.to change(Contact, :count).by(0)
       end
 
-      it 'updates updated article' do
+      it "updates updated article" do
         patch :update, params: { id: contact.id, contact: update_attributes }, session: {}
         contact.reload
         expect(update_attributes[:full_name]).to eq('Larry')
@@ -98,6 +98,26 @@ RSpec.describe ContactsController, type: :controller do
         expect(update_attributes[:address]).to eq('abcdefg')
       end
 
+      it "redirects the :create template" do
+        patch :update, params: { id: contact.id, contact: update_attributes }, session: {}
+        contact = Contact.last
+        expect(response).to redirect_to(contact_path(contact))
+      end
+
+    end
+
+  describe 'DELETE #destroy' do
+    let!(:contact) {create(:contact)}
+    it 'deletes the contact' do
+      expect do
+        delete :destroy, params: { id: contact.id }, session: {}
+      end.to change(Contact, :count).by(-1)
+    end
+
+    it 'redirects the :index template' do
+      delete :destroy, params: { id: contact.id }, session: {}
+      expect(response).to redirect_to(contacts_path)
     end
   end
+
 end
